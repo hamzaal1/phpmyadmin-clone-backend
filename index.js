d9fe1f539626fb65e14feee4a730019d504d3109
hamza <hamzademnati21@gmail.com>
2023-08-14 10:54:42 +0100

    all needed enppoint done need to clean code littlebite

--- index.js
+++ index.js
@@ -85,6 +85,57 @@ app.post("/databases/expande/table/delete", async (req, res) => {
     });
     mysql.end();
 });
+app.post("/databases/expande/table/add", async (req, res) => {
+    const { mysql } = req;
+    const { table_name, rows } = req.body;
+
+    if (!rows.length) return res.status(301).json({ message: "table must have more than one colmun", status: 0 });;
+    const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS ${table_name} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            ${rows.map(row => {
+        if (row.type === 'date') {
+            return `${row.name} ${row.type} DEFAULT CURRENT_DATE`;
+        }
+        return `${row.name} ${row.type}`;
+    }).join(', ')}
+        );
+    `;
+    mysql.query(createTableQuery, function (err, result, fields) {
+        if (err) res.status(500).json({ error: 'An error occurred' });
+        res.json({ message: "table add successfully", status: 1 });
+    });
+    mysql.end();
+});
+app.post("/databases/expande/table/add/record", async (req, res) => {
+    const { mysql } = req;
+    const { table, records } = req.body;
+
+    if (!records.length) return res.status(301).json({ message: "table must have more than one colmun", status: 0 });;
+    const insertQuery = `
+    INSERT INTO ${table} (${records.map(record => record.field).join(', ')})
+    VALUES (${records.map(record => '?').join(', ')})
+  `;
+    const values = records.map(record => record.value);
+
+    mysql.query(insertQuery, values, function (err, result, fields) {
+        if (err) res.status(500).json({ error: 'An error occurred' });
+        res.json({ message: "record add successfully", status: 1 });
+    });
+    mysql.end();
+});
+
+
+app.post("/databases/expande/table/drop", async (req, res) => {
+    const { mysql } = req;
+    const { table_name } = req.body;
+
+    mysql.query(`DROP TABLE IF EXISTS ${table_name}`, function (err, result, fields) {
+        if (err) res.status(500).json({ error: 'An error occurred' });
+        res.json({ message: "table add successfully", status: 1 });
+    });
+    mysql.end();
+});
 
 
 app.listen(process.env.port || PORT, () => {
